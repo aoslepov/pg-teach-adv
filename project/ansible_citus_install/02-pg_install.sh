2b567a7461e8c23d5ce41c11826d9c71fd677faf
Alexander <ao.slepov@gmail.com>
2024-01-16 19:41:23 +0300

    pg-adv

--- project/ansible_citus_install/02-pg_install.sh
+++ project/ansible_citus_install/02-pg_install.sh
@@ -1,4 +1,4 @@
-#export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -u ubuntu -i hosts etcd.yml -e "permit_root_login=yes" -b 
-#export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -u ubuntu -i hosts citus.yml -e "permit_root_login=yes" -b 
+export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -u ubuntu -i hosts etcd.yml -e "permit_root_login=yes" -b 
+export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -u ubuntu -i hosts citus.yml -e "permit_root_login=yes" -b 
 export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -u ubuntu -i hosts haproxy.yml -e "permit_root_login=yes" -b
-#export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -u ubuntu -i hosts monitoring.yml -e "permit_root_login=yes"
+export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -u ubuntu -i hosts monitoring.yml -e "permit_root_login=yes"
